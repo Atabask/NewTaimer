3506c4b8ded8e8801b75a9b4cd9c2d24e962332b
Atabask <87390618+Atabask@users.noreply.github.com>
2022-01-17 13:37:56 +0300

    master

---
+++ js/utils/getTimer.js
@@ -0,0 +1,30 @@
+import {Howl, Howler} from '../../node_modules/howler/dist/howler.js';
+
+const sound = new Howl({
+  src: '../audio/audio.mp3'
+});
+
+
+sound.play();
+
+Howler.volume(0.5);
+
+window.startTimer = function() {
+    let timer = document.getElementById("timer");   
+    
+
+    if (timer.value == 0) {
+        sound.play();
+        return; 
+    } else {
+        document.getElementById("timer").innerHTML = timer.value --;
+        setTimeout(startTimer,1000);
+    }
+}
+
+window.stopTimer = function() {
+    clearTimeout(timer.value);
+    timer.value = 0;
+}
+
+
